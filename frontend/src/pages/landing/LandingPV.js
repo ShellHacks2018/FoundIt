e6cc646cb23ed325dbc77fe66a84dfe7d2649190
asalinasj <andressalinasj@gmail.com>
2018-09-06 00:00:15 -0400

    Navbar
    Navbar implementation with materials ui

--- frontend/src/pages/landing/LandingPV.js
+++ frontend/src/pages/landing/LandingPV.js
@@ -3,22 +3,66 @@ import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu';
+import { Redirect, Link } from 'react-router-dom';
+
+//import style from './LandingPV.css'
 
 const styles = {
   card: {
     maxWidth: '275px',
-    color: 'salmon'
+    color: 'salmon',
+    display: 'block',
+
+  },
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flexGrow: 1,
+    //marginLeft: 'auto',
+    textAlign: 'left',
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  loginButton: {
+    marginRight: 50,
+
   }
+  
+
 }
 
 const LandingPV = props => {
   const { classes } = props
   return (
-    <Card className={classes.card}>
-      <CardContent>
-        <h1>Sign In Should Go Here</h1>
-      </CardContent>
-    </Card>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="title" color="inherit" className={classes.flex}>
+            Fount It!
+          </Typography>
+          <Link to='/map'>
+            <Button className={classes.loginButton} color="inherit">Login</Button>
+          </Link>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Card className={classes.card}>
+        <CardContent>
+          <h1>Sign In Should Go Here</h1>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/map/mapPC.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import mapPV from './mapPV';
+
+class mapPC extends React.Component {
+    render(){
+        return(
+            <div>
+                <mapPV />
+            </div>
+        );
+    }
+}
+
+export default mapPC
