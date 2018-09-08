45b21e48593d833fb1131fdd0eb0d746c1b27aad
Mauricio <msant323@fiu.edu>
2018-09-08 13:42:51 -0400

    basic AddItem component wip

---
+++ frontend/src/components/AddItem/AddItemC.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import AddItemV from './AddItemV'
+
+class AddItemC extends Component {
+
+  render () {
+    return (
+      <div>
+        <AddItemV />
+      </div>
+    )
+  }
+}
+
+export default AddItemC
---
+++ frontend/src/components/AddItem/AddItemV.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import CamSnapshotC from '../camSnapshot/camSnapshotC'
+
+import Paper from '@material-ui/core/Paper';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+
+const AddItemV = (props) => {
+    return(
+        <div>
+        </div>
+    );
+}
+
+export default AddItemV;
