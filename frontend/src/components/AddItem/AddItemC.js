0d3f9c7227a38d54fc111a7edb6e43e2c2a9baea
Mauricio <msant323@fiu.edu>
2018-09-08 18:53:30 -0400

    add icons vs text on GetImageV, AddItemV dislpay done, functionality wip

--- frontend/src/components/AddItem/AddItemC.js
+++ frontend/src/components/AddItem/AddItemC.js
@@ -3,11 +3,45 @@ import React, { Component } from 'react'
 import AddItemV from './AddItemV'
 
 class AddItemC extends Component {
+  state = {
+    formOpen: false,
+    checkboxes: {
+      Clothing: false,
+      Jewelery: false,
+      Electronics: false,
+      Book: false,
+      Other: false
+    }
+  }
+
+  formOpenCB = () => {
+    this.setState({ formOpen: true });
+  };
+
+  formCloseCB = () => {
+    this.setState({ formOpen: false });
+  };
+
+  checkClicked = (name) => event => {
+    // [name] is called "computed property name"
+    // Allows for variablly setting state in a single fuction!
+    this.setState({ ...this.state, 
+                    checkboxes: {
+                      ...this.state.checkboxes,
+                      [name]: event.target.checked }
+                  });
+  };
 
   render () {
     return (
       <div>
-        <AddItemV />
+        <AddItemV 
+          form={this.state.formOpen}
+          formOpenCB = {this.formOpenCB}
+          formCloseCB = {this.formCloseCB}
+          checkedState = {this.state['checkboxes']}
+          checkClicked = {this.checkClicked}
+        />
       </div>
     )
   }
